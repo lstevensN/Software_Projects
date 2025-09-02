9b6ebfb65de3cd3d45d474769e80c060abccf4c7
lstevensN <lstevens@student.neumont.edu>
2025-09-02 17:46:57 -0600

    Replaced wails main.js file

--- Paisley/index.js
+++ Paisley/index.js
@@ -1,12 +1,14 @@
-;(async () => {
-  const SubscribersHandler = require('./classes/SubscribersHandler.js')
-  const sh = SubscribersHandler.init()
-  await sh.load()
-  await sh.start()
+async function paisley() {
+  (async () => {
+    const SubscribersHandler = require('./classes/SubscribersHandler.js')
+    const sh = SubscribersHandler.init()
+    await sh.load()
+    await sh.start()
 
-  const MailHandler = require('./classes/MailHandler.js')
-  const mh = MailHandler.init()
-  await mh.start()
-})()
-  .then(() => console.log('all done'))
-  .catch((e) => console.log(e))
+    const MailHandler = require('./classes/MailHandler.js')
+    const mh = MailHandler.init()
+    await mh.start()
+  })()
+    .then(() => console.log('all done'))
+    .catch((e) => console.log(e))
+}
