0fad7b7b5af77a6892daad3e480ae0b4528c9886
lstevensN <lstevens@student.neumont.edu>
2025-09-03 06:30:20 -0600

    Added require.js

--- Paisley/index.js
+++ Paisley/index.js
@@ -1,14 +1,12 @@
-async function paisley() {
-  (async () => {
-    const SubscribersHandler = require('./classes/SubscribersHandler.js')
-    const sh = SubscribersHandler.init()
-    await sh.load()
-    await sh.start()
+(async () => {
+  const SubscribersHandler = require('./classes/SubscribersHandler.js')
+  const sh = SubscribersHandler.init()
+  await sh.load()
+  await sh.start()
 
-    const MailHandler = require('./classes/MailHandler.js')
-    const mh = MailHandler.init()
-    await mh.start()
-  })()
-    .then(() => console.log('all done'))
-    .catch((e) => console.log(e))
-}
+  const MailHandler = require('./classes/MailHandler.js')
+  const mh = MailHandler.init()
+  await mh.start()
+})()
+  .then(() => console.log('all done'))
+  .catch((e) => console.log(e))
